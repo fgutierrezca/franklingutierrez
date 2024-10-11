9ffa463880c22b6d37150bed64f2b83f776c8b66
fgutierrezca <139795411+fgutierrezca@users.noreply.github.com>
2024-10-10 18:39:39 -0600

    Corrección de errores
    Se corrigieron diversos errores en el html y css. Como que el navbar este fijo, y mejoras en las clases importadas de tailwind. Además de comenzar a hacer responsive el portafolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			'sm': '100px',
+			// => @media (min-width: 640px) { ... }
+
+			'md': '401px',
+			// => @media (min-width: 768px) { ... }
+		},
 		extend: {},
 	},
 	plugins: [],
